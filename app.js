9c7271d1d504484809c02a4078eeb4aa0b92caa2
Casey McCaskill <>
2017-06-28 15:39:13 -0400

    wtfm8

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const models = require('./models');
+const routes = require('./routes');
+const fs = require('fs');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache')
+app.set('layout', 'layout');
+app.use('/static', express.static('static'));
 
-app.get('/', function(req,res){
-	res.send('Done');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+app.use(routes)
 
 app.listen(3000,function(req,res){
 	console.log('App is running on localhost:3000');
---
+++ routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const models = require("./models");
+
+router.get('/', function(req,res){
+	res.send('Done');
+});
